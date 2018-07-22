fa6c6f1efb5f511f20422d1edc0070620714c7a9
Lionel Flandrin <lionel@svkt.org>
2018-07-22 23:36:29 +0200

    Fix floating mouse support

--- dwm.c
+++ dwm.c
@@ -926,7 +926,7 @@ grabbuttons(Client *c, int focused)
 					XGrabButton(dpy, buttons[i].button,
 						buttons[i].mask | modifiers[j],
 						c->win, False, BUTTONMASK,
-						GrabModeSync, GrabModeSync, None, None);
+						GrabModeAsync, GrabModeAsync, None, None);
 	}
 }
 
