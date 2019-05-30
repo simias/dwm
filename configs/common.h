dbe180e3c62ebca0aa1d7c24bfd96967f9d1feb1
Lionel Flandrin <lionel@svkt.org>
2019-05-30 15:13:04 +0100

    Add support for dmenu with history

--- configs/common.h
+++ configs/common.h
@@ -49,7 +49,16 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run",
+				  "-H", "/home/lionel/.dmenu-history",
+				  "-m", dmenumon,
+				  "-fn", dmenufont,
+				  "-nb", col_gray1,
+				  "-nf", col_gray3,
+				  "-sb", col_cyan,
+				  "-sf", col_gray4,
+				  NULL };
+
 static const char *termcmd[]  = { "st", NULL };
 
 #ifndef XK_XF86AudioLowerVolume
