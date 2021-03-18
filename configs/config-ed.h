77a8191ba13e7235f4660aa511e4c53948e826e3
Lionel Flandrin <lfg@svkt.eu>
2021-03-18 23:11:52 +0000

    Update placement rules

--- configs/config-ed.h
+++ configs/config-ed.h
@@ -5,23 +5,24 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class                   instance    title       tags mask  isfloating   monitor */
-	{ "Navigator",             NULL,       NULL,       1 << 7,    0,           0 },
-	{ "Firefox",               NULL,       NULL,       1 << 7,    0,           0 },
-	{ "firefox",               NULL,       NULL,       1 << 7,    0,           0 },
-	{ "mainterm",              NULL,       NULL,       1 << 6,    0,           0 },
-	{ "Emacs",                 NULL,       NULL,       1 << 1,    0,           1 },
-	{ "gvim",                  NULL,       NULL,       1 << 0,    0,           1 },
-	{ "Gvim",                  NULL,       NULL,       1 << 0,    0,           1 },
-	{ "SpiderOakONE",          NULL,       NULL,       1 << 4,    0,           0 },
-	{ "ProtonMail",            NULL,       NULL,       1 << 4,    0,           0 },
-	{ "Thunderbird",           NULL,       NULL,       1 << 5,    0,           0 },
-	{ "VirtualBox Machine",    NULL,       NULL,       1 << 1,    0,           0 },
-	{ "VirtualBox Manager",    NULL,       NULL,       1 << 1,    0,           0 },
-	{ "VirtualBox Manager",    NULL,       NULL,       1 << 1,    0,           0 },
-	{ "Telegram",              NULL,       NULL,       1 << 8,    0,           0 },
-	{ "Signal",                NULL,       NULL,       1 << 8,    0,           0 },
-	{ "Spotify",               NULL,       NULL,       1 << 8,    0,           0 },
+	/* class                   instance      title       tags mask  isfloating   monitor */
+	{ "Navigator",             NULL,         NULL,       1 << 7,    0,           0 },
+	{ "firefox",               NULL,         NULL,       1 << 7,    0,           0 },
+	{ "Bitwarden",             NULL,         NULL,       1 << 3,    0,           0 },
+	{ "mainterm",              NULL,         NULL,       1 << 6,    0,           0 },
+	{ "Emacs",                 NULL,         NULL,       1 << 1,    0,           1 },
+	{ "gvim",                  NULL,         NULL,       1 << 0,    0,           1 },
+	{ "Gvim",                  NULL,         NULL,       1 << 0,    0,           1 },
+	{ "SpiderOakONE",          NULL,         NULL,       1 << 4,    0,           0 },
+	{ "ProtonMail",            NULL,         NULL,       1 << 4,    0,           0 },
+	{ "Thunderbird",           "Msgcompose", NULL,       1 << 0,    0,           1 },
+	{ "Thunderbird",           NULL,         NULL,       1 << 5,    0,           0 },
+	{ "VirtualBox Machine",    NULL,         NULL,       1 << 1,    0,           0 },
+	{ "VirtualBox Manager",    NULL,         NULL,       1 << 1,    0,           0 },
+	{ "VirtualBox Manager",    NULL,         NULL,       1 << 1,    0,           0 },
+	{ "Telegram",              NULL,         NULL,       1 << 8,    0,           0 },
+	{ "Signal",                NULL,         NULL,       1 << 8,    0,           0 },
+	{ "Spotify",               NULL,         NULL,       1 << 8,    0,           0 },
 };
 
 #include "configs/common.h"
