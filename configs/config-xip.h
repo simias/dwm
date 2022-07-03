18bda91f4d06e6e0c77fd9a27af104f81499572b
Lionel Flandrin <lionel@svkt.org>
2022-07-03 14:58:28 +0100

    Update xip config

--- configs/config-xip.h
+++ configs/config-xip.h
@@ -6,10 +6,21 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class          instance    title       tags mask  isfloating   monitor */
-	{ "Firefox",      NULL,       NULL,       1 << 7,    0,           -1 },
-	{ "mainterm",     NULL,       NULL,       1 << 6,    0,           -1 },
-	{ "Emacs",        NULL,       NULL,       1 << 5,    0,           -1 },
-	{ "SpiderOakONE", NULL,       NULL,       1 << 4,    0,           -1 },
+	{ "Navigator",             NULL,         NULL,       1 << 7,    0,           0 },
+	{ "firefox",               NULL,         NULL,       1 << 7,    0,           0 },
+	{ "Bitwarden",             NULL,         NULL,       1 << 3,    0,           0 },
+	{ "mainterm",              NULL,         NULL,       1 << 6,    0,           0 },
+	{ "Emacs",                 NULL,         NULL,       1 << 1,    0,           1 },
+	{ "gvim",                  NULL,         NULL,       1 << 0,    0,           1 },
+	{ "Gvim",                  NULL,         NULL,       1 << 0,    0,           1 },
+	{ "ProtonMail",            NULL,         NULL,       1 << 4,    0,           0 },
+	{ "Thunderbird",           NULL,         NULL,       1 << 5,    0,           0 },
+	{ "VirtualBox Machine",    NULL,         NULL,       1 << 1,    0,           0 },
+	{ "VirtualBox Manager",    NULL,         NULL,       1 << 1,    0,           0 },
+	{ "VirtualBox Manager",    NULL,         NULL,       1 << 1,    0,           0 },
+	{ "Telegram",              NULL,         NULL,       1 << 8,    0,           0 },
+	{ "Signal",                NULL,         NULL,       1 << 8,    0,           0 },
+	{ "Spotify",               NULL,         NULL,       1 << 8,    0,           0 },
 };
 
 #include "configs/common.h"
